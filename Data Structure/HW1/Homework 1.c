ea9b31038f7bd4cd900b07a5b77e28844cc00733
YHWOO13 <59326872+YHWOO13@users.noreply.github.com>
2020-10-24 23:46:49 +0900

    Create Homework 1.c
    Copy of code given by professor

---
+++ Data Structure/HW1/Homework 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct student {
+
+	char name[20];
+	int year;
+	char dept[30];
+	int age;
+	struct sudent* next;
+};
+
+int main()
+{
+	int noofdata;
+	int i = 0;
+
+	FILE* fp;
+
+	struct student data[100];
+
+	fp = fopen("studentdata.txt", "r");
+
+	fscanf(fp, "%s %d %s %d", data[0].name, &data[0].year, data[0].dept, &data[0].age);
+
+	fclose(fp);
+
+	printf("%s, %d, %s, %d\n", data[0].name, data[0].year, data[0].dept, data[0].age);
+
+
+}
