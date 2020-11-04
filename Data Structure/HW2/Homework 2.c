14ed9f71f60d816d4d880360a30b9817c4f9d96c
YHWOO13 <59326872+YHWOO13@users.noreply.github.com>
2020-11-04 18:01:06 +0900

    Update Homework 2.c
    Draft

--- Data Structure/HW2/Homework 2.c
+++ Data Structure/HW2/Homework 2.c
@@ -1,14 +1,89 @@
 /* Author: Yoonhyuck WOO / JBNU_Industrial Information system Engineering
  Date; Nov. 11 2020
  Title: Student data in Linked List*/
- 
+
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
+#include<string.h>
 
-int main() 
+typedef struct student* listPointer;
+typedef struct student
 {
-	
-	
+	int no;
+	char name[20];
+	int year;
+	char department[30];
+	int age;
+	listPointer* next;
+}student;
+
+listPointer head = NULL;
+
+int add_stduent_info(int no, char* name, int year, char* department, int age)
+{
+	listPointer stu = (student*)malloc(sizeof(student));
+	listPointer trace = head;
+	stu->no = no;
+	strcpy_s(stu->name, sizeof(name), name);
+	stu->year = year;
+	strcpy_s(stu->department, sizeof(department), department);
+	stu->age = age;
+	stu->next = NULL;
+	if (head) {
+		while (trace->next) {
+			trace = trace->next;
+		}
+		trace->next = stu;
+	}
+	else {
+		head = stu;
+	}
+}
+
+void visualize() {
+	listPointer visual = head;
+	for (; visual; visual = visual->next) {
+		printf("Std.No: %d, Std.Name: %s, Year: %d, Department: %s, Age: %d \n", visual->no, visual->name, visual->year, visual->department, visual->age);
+	}
+}
+
+int main()
+{
+	int i = 1, j = 0, Std_no = 0, year = 0, age = 0, answer = 0;
+	char Std_name[30], department[30];
+	listPointer trace = head;
+	//listPointer stu = (student*)malloc(sizeof(student));
+	printf("Choose the option: \n");
+	while (1)
+	{
+		printf("1. Add information of student.\n");
+		printf("2. Quit.\n");
+		scanf_s("%d", &answer);
+		printf("Answer: %d\n", answer);
+		if (answer == 1)
+		{
+			printf("             =============Adding information of student.=============\n");
+			printf("%d. Std.No: ", i);
+			scanf_s("%d", &Std_no);
+			printf("%d. Std.Name: ", i);
+			scanf_s("%s", &Std_name, sizeof(Std_name));
+			printf("%d. year: ", i);
+			scanf_s("%d", &year);
+			printf("%d. department: ", i);
+			scanf_s("%s", department, sizeof(department));
+			printf("%d. age: ", i);
+			scanf_s("%d", &age);
+			printf("\n");
+			i = i + 1;
+			add_stduent_info(Std_no, Std_name, year, department, age);
+		}
+		else if (answer == 2)
+		{
+			visualize();
+			printf("             =============End this program.=============\n");
+			break;
+		}
+		else { printf("Out of order"); }
+	}
 	return 0;
 }
