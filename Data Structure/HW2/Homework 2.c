e41eba16b3a339c1a78235488b7b079e13d5dc9b
YHWOO13 <59326872+YHWOO13@users.noreply.github.com>
2020-11-04 12:08:02 +0900

    Create Homework 2.c
    Add information about author, starting date, and title

---
+++ Data Structure/HW2/Homework 2.c
@@ -0,0 +1,14 @@
+/* Author: Yoonhyuck WOO / JBNU_Industrial Information system Engineering
+ Date; Nov. 11 2020
+ Title: Student data in Linked List*/
+ 
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() 
+{
+	
+	
+	return 0;
+}
