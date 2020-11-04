91fecd55076468e68949edd989efae5e59cb7fe6
YHWOO13 <59326872+YHWOO13@users.noreply.github.com>
2020-11-04 14:01:14 +0900

    Update Homework 1.c
    There is some confusion. So, change the code

--- Data Structure/HW1/Homework 1.c
+++ Data Structure/HW1/Homework 1.c
@@ -1,33 +1,33 @@
 /* Author: Yoonhyuck WOO / JBNU_Industrial Information system Engineering
  Date; Oct. 19 2020
  Title: Student Array*/
-#include <stdio.h>
+#include<stdio.h>
 
-struct student {
-
-	char name[20];
-	int year;
-	char dept[30];
-	int age;
-	struct sudent* next;
-};
-
-int main()
+int main(void)
 {
-	int noofdata;
 	int i = 0;
+	struct student
+	{
+		int no;
+		char name[20];
+		int year;
+		char department[30];
+		int age;
+	};
 
 	FILE* fp;
+	fopen_s(&fp, "studentdata.txt", "r");
 
-	struct student data[100];
+	struct student stu[24];
 
-	fp = fopen("studentdata.txt", "r");
+	for (i = 0; i < 24; i++)
+	{
+		fscanf_s(fp, "%d %s %d %s %d", &stu[i].no, stu[i].name, sizeof(stu[i].name), &stu[i].year, stu[i].department, sizeof(stu[i].department), &stu[i].age);
 
-	fscanf(fp, "%s %d %s %d", data[0].name, &data[0].year, data[0].dept, &data[0].age);
-
+		printf("no.%2d Std.ID: %d Name: %s, Year: %d, Department: %s, Age: %d",
+			i + 1, stu[i].no, &stu[i].name, stu[i].year, stu[i].department, stu[i].age);
+		printf("\n");
+	}
 	fclose(fp);
-
-	printf("%s, %d, %s, %d\n", data[0].name, data[0].year, data[0].dept, data[0].age);
-
-
+	return 0;
 }
