0c23ae832fbb708d19cf872aea3addbc6355c8a7
YHWOO13 <59326872+YHWOO13@users.noreply.github.com>
2020-11-05 10:01:46 +0900

    Update Homework 2.c
    annotation

--- Data Structure/HW2/Homework 2.c
+++ Data Structure/HW2/Homework 2.c
@@ -7,6 +7,8 @@
 #include<string.h>
 
 typedef struct student* listPointer;
+/*문자열을 받을 때 char* name은 메모리 할당을 또 따로 해주어야 함= 번거로움
+밑에와 같이 배열로 받으면 그럴 필요가 없음.*/
 typedef struct student
 {
 	int no;
@@ -18,7 +20,8 @@ typedef struct student
 }student;
 
 listPointer head = NULL;
-
+/*문자열을 다른 함수에 넘겨주려면 문자열의 첫번째 주소를 알려줘야함
+ 그렇기 떄문에 함수의 매개변수로 포인터 변수를 씀*/
 int add_stduent_info(int no, char* name, int year, char* department, int age)
 {
 	listPointer stu = (student*)malloc(sizeof(student));
@@ -52,7 +55,6 @@ int main()
 	int i = 1, j = 0, Std_no = 0, year = 0, age = 0, answer = 0;
 	char Std_name[100], department[100];
 	listPointer trace = head;
-	//listPointer stu = (student*)malloc(sizeof(student));
 	printf("Choose the option: \n");
 	while (1)
 	{
@@ -75,6 +77,9 @@ int main()
 			scanf_s("%d", &age);
 			printf("\n");
 			i = i + 1;
+
+			/*만약 함수에서 매개변수로 포인터변수를 썼다면 main에서 포인터 변수를 받기 위해
+			&붙여야하지만 구조체를 만들때 배열로 선언했기떄문에 변수이름만 써줘도 된다.*/
 			add_stduent_info(Std_no, Std_name, year, department, age);
 		}
 		else if (answer == 2)
@@ -86,5 +91,6 @@ int main()
 		}
 		else { printf("             =============Out of order.=============\n"); }
 	}
+	free(head);
 	return 0;
 }
